f733a9f6341f762cc31eaf082c3d54ac335effc8
Wesam Abadi <wisamabady7@gmail.com>
2024-02-29 14:01:21 +0100

    add connection and json read

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$servername = "localhost";
-$username = "username";
-$password = "";
-$dbname = "bookshop";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+<body>
+    <h2>shop data</h2>
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    <?php
+    require_once('db_config.php');
 
-$conn->close();
-?>
+    $jsonData = file_get_contents('data.json');
+    $data = json_decode($jsonData, true);
+
+    foreach ($data as $value) {
+        echo $value['product_name'] . "<br>";
+    }
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+
+    $conn->close();
+    ?>
+</body>
+
+</html>
